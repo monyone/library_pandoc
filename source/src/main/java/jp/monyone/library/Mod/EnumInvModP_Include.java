97796dd69719515a5b5601c61e680d1f524c4e60
monyone <monyone.teihen@gmail.com>
2015-07-02 03:16:06 +0900

    Fix EnumModInvP signature

--- source/src/main/java/jp/monyone/library/Mod/EnumInvModP_Include.java
+++ source/src/main/java/jp/monyone/library/Mod/EnumInvModP_Include.java
@@ -4,12 +4,14 @@ import static jp.monyone.library.Mod.ModPowRec_Include.mod_pow;
 
 public class EnumInvModP_Include {
 	//@start
-	// inv[1] ... inv[N] にそれぞれの mod P での逆元が入る
-	public static void mod_inv(long[] inv, int N, long p){
+	// 返戻値(inv[1] ... inv[N]) にそれぞれの mod P での逆元が入る
+	public static long[] mod_inv(int N, long p){
+		long[] inv = new long[N + 1];
 		inv[1] = 1;
 		for(int i = 2; i <= N; i++){
 			inv[i] = p - (p / i) * inv[(int)(p % i)] % p;
 		}
+		return inv;
 	}
 	//@end
 }
