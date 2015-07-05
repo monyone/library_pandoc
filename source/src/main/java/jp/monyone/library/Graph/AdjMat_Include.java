e36840e725d8fe6e89f3508732bb00d77274e764
monyone <monyone.teihen@gmail.com>
2015-07-05 12:38:55 +0900

    Add adjMat for INF description.

--- source/src/main/java/jp/monyone/library/Graph/AdjMat_Include.java
+++ source/src/main/java/jp/monyone/library/Graph/AdjMat_Include.java
@@ -1,9 +1,11 @@
 package jp.monyone.library.Graph;
 
+import static jp.monyone.library.Template.LongINF_Include.INF;
+
 public class AdjMat_Include {
 	//@start
-	public static final long INF = Long.MAX_VALUE / 2 - 1;
-
+	// INF という値は適当に大きい値を突っ込んでおく.
+	// 二つ足しても平気な Long.MAX_VALUE / 2 - 1 を良く使う.
 	public static long[][] init_adj(final int n){
 		long[][] ret = new long[n][n];
 		for(int i = 0; i < n; i++){
