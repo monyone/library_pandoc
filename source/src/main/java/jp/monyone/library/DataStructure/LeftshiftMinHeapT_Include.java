9654fae991ffd39422ec0ab51d57dbb74938cade
monyone <monyone.teihen@gmail.com>
2015-07-13 23:09:12 +0900

    Rename Include source files

--- source/src/main/java/jp/monyone/library/DataStructure/LeftshiftMinHeapT_Include.java
+++ source/src/main/java/jp/monyone/library/DataStructure/LeftshiftMinHeapT_Include.java
@@ -1,17 +1,20 @@
 package jp.monyone.library.DataStructure;
 
-public class SkewMinHeapT {
+import java.util.Random;
+
+public class LeftshiftMinHeapT_Include {
 	//@start
-	public static class SkewHeap<T extends Comparable<? super T>> {
+	public static class LeftshiftHeap<T extends Comparable<? super T>> {
 
 		private static class Node<T> {
 			Node<T> left, right;
 			T value; // value に null を入れると死ぬ.
+			int s;
 
-			public Node(T value){ this.value = value; }
+			public Node(T value){ this.value = value; this.s = 1; }
 			public Node(Node<T> n){
 				this.left = n.left; this.right = n.right;
-				this.value = n.value;
+				this.value = n.value; this.s = n.s;
 			}
 
 			public String toString(){ // これはデバッグ用なので削ってもいい.
@@ -23,8 +26,12 @@ public class SkewMinHeapT {
 			if(a == null){ return b; }
 			if(b == null){ return a; }
 			if(a.value.compareTo(b.value) > 0){ return meld(b, a); } // > で Min, < で Max
+
 			a.right = meld(a.right, b);
-			{ Node<T> tmp = a.left; a.left = a.right; a.right = tmp; } // swapするだけ!
+			if(a.left == null || a.left.s < a.right.s){
+				Node<T> tmp = a.left; a.left = a.right; a.right = tmp;
+			}
+			a.s = (a.right == null ? 0 : a.right.s) + 1; //交換してるから, rightの方が低い
 			return a;
 		}
 
@@ -32,7 +39,7 @@ public class SkewMinHeapT {
 		// 基本的な操作
 		public void add(T value){ root = meld(root, new Node<T>(value)); }
 		public void pop(){ root = meld(root.left, root.right);}
-		public void merge(SkewHeap<T> other){ root = meld(this.root, other.root); }
+		public void merge(LeftshiftHeap<T> other){ root = meld(this.root, other.root); }
 		// ユーティリティ
 		public T poll(){ T val = peek(); pop(); return val; }
 		public T peek(){ return root.value; }
--- source/src/main/java/jp/monyone/library/DataStructure/SkewMinHeapT_Include.java
+++ source/src/main/java/jp/monyone/library/DataStructure/SkewMinHeapT_Include.java
@@ -1,20 +1,17 @@
 package jp.monyone.library.DataStructure;
 
-import java.util.Random;
-
-public class LeftshiftMinHeapT {
+public class SkewMinHeapT_Include {
 	//@start
-	public static class LeftshiftHeap<T extends Comparable<? super T>> {
+	public static class SkewHeap<T extends Comparable<? super T>> {
 
 		private static class Node<T> {
 			Node<T> left, right;
 			T value; // value に null を入れると死ぬ.
-			int s;
 
-			public Node(T value){ this.value = value; this.s = 1; }
+			public Node(T value){ this.value = value; }
 			public Node(Node<T> n){
 				this.left = n.left; this.right = n.right;
-				this.value = n.value; this.s = n.s;
+				this.value = n.value;
 			}
 
 			public String toString(){ // これはデバッグ用なので削ってもいい.
@@ -26,12 +23,8 @@ public class LeftshiftMinHeapT {
 			if(a == null){ return b; }
 			if(b == null){ return a; }
 			if(a.value.compareTo(b.value) > 0){ return meld(b, a); } // > で Min, < で Max
-
 			a.right = meld(a.right, b);
-			if(a.left == null || a.left.s < a.right.s){
-				Node<T> tmp = a.left; a.left = a.right; a.right = tmp;
-			}
-			a.s = (a.right == null ? 0 : a.right.s) + 1; //交換してるから, rightの方が低い
+			{ Node<T> tmp = a.left; a.left = a.right; a.right = tmp; } // swapするだけ!
 			return a;
 		}
 
@@ -39,7 +32,7 @@ public class LeftshiftMinHeapT {
 		// 基本的な操作
 		public void add(T value){ root = meld(root, new Node<T>(value)); }
 		public void pop(){ root = meld(root.left, root.right);}
-		public void merge(LeftshiftHeap<T> other){ root = meld(this.root, other.root); }
+		public void merge(SkewHeap<T> other){ root = meld(this.root, other.root); }
 		// ユーティリティ
 		public T poll(){ T val = peek(); pop(); return val; }
 		public T peek(){ return root.value; }
