1190d570189fa719c06aee1e05ad81b634d9969c
monyone <monyone.teihen@gmail.com>
2015-07-13 23:18:38 +0900

    fix invalid package name

--- verify/src/main/java/jp/monyone/verify/AOJ/DSL2A/RangeMinimumQuerySparseTableBitLookupTable/Main.java
+++ verify/src/main/java/jp/monyone/verify/AOJ/DSL2A/RangeMinimumQuerySparseTableBitLookupTable/Main.java
@@ -1,4 +1,4 @@
-package jp.monyone.verify.AOJ.DSL2A.RMQ;
+package jp.monyone.verify.AOJ.DSL2A.RangeMinimumQuerySparseTableBitLookupTable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
