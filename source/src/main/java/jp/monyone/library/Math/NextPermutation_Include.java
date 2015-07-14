1793bace48f7024e04fd3f80d34d3b0354dab916
monyone <monyone.teihen@gmail.com>
2015-07-15 02:56:27 +0900

    refactore NextPermutation for compile warning

--- source/src/main/java/jp/monyone/library/Math/NextPermutation_Include.java
+++ source/src/main/java/jp/monyone/library/Math/NextPermutation_Include.java
@@ -6,6 +6,7 @@ import static jp.monyone.library.Template.ReverseArrayT_Include.reverse;
 public class NextPermutation_Include {
 	//@start
 	// 任意の Comparable<T> を実装したクラスに対する next_permutation
+	@SafeVarargs
 	public static <T extends Comparable<? super T>> boolean next_permutation(T ... perm) {
 		for(int i = perm.length - 1; i > 0; i--){
 			if(perm[i - 1].compareTo(perm[i]) < 0){
