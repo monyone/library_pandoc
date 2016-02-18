ef245d5ab51457041bdc902dd50ffbaba1753920
monyone <monyone.teihen@gmail.com>
2016-02-18 23:53:20 +0900

    fix ChineseRemainder_Garner overflow and MOD

--- source/src/main/java/jp/monyone/library/Mod/ChineseRemainder_Garner_Include.java
+++ source/src/main/java/jp/monyone/library/Mod/ChineseRemainder_Garner_Include.java
@@ -4,21 +4,40 @@ import static jp.monyone.library.Mod.InvModM_Include.mod_inv;
 
 public class ChineseRemainder_Garner_Include {
 	//@start
+	// O(N^2). ms[i] は互いに素でなくてはならない.
+	// Π(ms[i]) がオーバーフローする場合でも, 最後に適切に MOD を取れば OK
 	public static long chinese_remainder(long[] as, long[] ms){
-		long prod = 1, sum = 0;
-		long v = 0;
+		long[] vs = new long[ms.length];
+		long[] sums = new long[ms.length];
+		long[] invs = new long[ms.length];
 
 		for(int i = 0; i < ms.length; i++){
-			long sub = as[i] - (sum % ms[i]);
-			if(sub < 0){ sub += ms[i]; }
 
-			v = (sub * mod_inv(prod, ms[i])) % ms[i];
+			invs[i] = 1; // mod(ms[i]) の中で 1 / (ms[0] * ms[1] ... ms[i - 1]) を求める
+			sums[i] = as[i]; // mod(ms[i]) の中で, a[i] - v[0] - v[1]ms[0] ... を求める
 
-			sum += (v * prod);
-			prod *= ms[i];
+			long mult = 1; // mod(ms[i]) の中で, ms[0] * ... * ms[i - 2] を求める
+			for(int j = 0; j < i; j++){
+				sums[i] -= (vs[j] * mult) % ms[i];
+				if(sums[i] < 0){ sums[i] += ms[i]; }
+
+				invs[i] = (invs[i] * mod_inv(ms[j], ms[i])) % ms[i];
+				mult = (mult * ms[j]) % ms[i];
+			}
+
+			vs[i] = (sums[i] * invs[i]) % ms[i];
 		}
 
-		return sum;
+		// MOD を取る前に 0 かどうかを知る場合, ここで ∀i.(vs[i] == 0) チェック
+
+		long ret = 0;
+		for(int i = 0; i < ms.length; i++){
+			long mult = 1;
+			for(int j = 0; j < i; j++){ mult *= ms[j]; }// ここで MOD
+			ret += vs[i] * mult;// ここで MOD
+		}
+
+		return ret;
 	}
 	//@end
 
